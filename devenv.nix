4ed6c397ffa21ab411809d6d19e452e040943f55
igor-primo <igorprimo62@gmail.com>
2024-11-09 10:25:01 -0300

    ops: add nightly rust version

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,14 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [ pkgs.git ];
+  packages = [ 
+    pkgs.git
+  ];
 
-  languages.rust.enable = true;
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+  };
 
   pre-commit.hooks.rustfmt.enable = true;
 }
