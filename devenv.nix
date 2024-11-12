c66bf8810b56813f25bef53633487905a3527635
igor-primo <igorprimo62@gmail.com>
2024-11-12 08:26:14 -0300

    feat: add nightly components and targets

--- devenv.nix
+++ devenv.nix
@@ -3,11 +3,21 @@
 {
   packages = [ 
     pkgs.git
+    pkgs.rustup
   ];
 
   languages.rust = {
     enable = true;
     channel = "nightly";
+    components = [ 
+      "rust-src"
+      "cargo"
+      "rustc"
+      "llvm-tools-preview"
+    ];
+    targets = [
+      "x86_64-unknown-none"
+    ];
   };
 
   pre-commit.hooks.rustfmt.enable = true;
