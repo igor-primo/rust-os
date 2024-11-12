0f561653de45ccf83a6ba212d98c3206e83f0efb
igor-primo <igorprimo62@gmail.com>
2024-11-12 10:01:31 -0300

    feat: complete vga text mode module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std] // Do not use standard library
 #![no_main] // Do not use the normal entry point chain. We will overwrite crt0 entry point directly.
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
@@ -8,19 +10,21 @@ static HELLO: &[u8] = b"Hello World!";
 #[no_mangle] // Disable name mangling
              // Also, use the C calling convention
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    println!("Hello World{}", "!");
+    panic!("ui");
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
