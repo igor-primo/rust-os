3cabde65603a310981ca476496b3b1cc66853b3d
igor-primo <igorprimo62@gmail.com>
2024-11-19 13:21:20 -0300

    feat: add double fault handler

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,9 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    };
 
     #[cfg(test)]
     test_main();
